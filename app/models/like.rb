9670c05a472499a1a66b3d12abe6fe40807932b3
kian1213 <chrono0424@gmail.com>
2016-04-25 15:14:10 +0800

    add like to activities

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,15 @@
 class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :activity
+
+  class << self
+    def current_like activity_id, user_id
+      self.where activity_id: activity_id, user_id: user_id
+    end
+
+    def check_if_included? activity_id, user_id
+      like = self.where activity_id: activity_id, user_id: user_id
+      !like.first.nil?
+    end
+  end
 end
