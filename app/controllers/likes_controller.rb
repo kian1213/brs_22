6afa429922e00541a2edd1b5046dd31317dd4e33
kian1213 <chrono0424@gmail.com>
2016-04-25 14:59:28 +0800

    add like

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,32 @@
+class LikesController < ApplicationController
+  def create
+    @like = Like.new like_params
+    @activity = Activity.find like_params[:activity_id]
+    user = User.find @activity.owner_id
+
+    if @like.save
+      respond_to do |format|
+        format.html {redirect_to user_path user}
+        format.js
+      end
+    else
+    end
+  end
+
+  def destroy
+    @like = Like.find params[:id]
+    @activity = Activity.find @like.activity_id
+    user = User.find @activity.owner_id
+    @like.destroy
+
+    respond_to do |format|
+      format.html {redirect_to user_path user}
+      format.js
+    end
+  end
+
+  private
+  def like_params
+    params.require(:like).permit :activity_id, :user_id
+  end
+end
